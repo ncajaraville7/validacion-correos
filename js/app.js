09773e26b5a4eac972975cff8b131d07bac7e612
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2021-11-24 21:36:51 -0300

    Continuamos con la validacion

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const btnEnviar = document.querySelector('#enviar');
 const inputEmail = document.querySelector('#email');
 const inputAsunto = document.querySelector('#asunto');
 const inputMensaje = document.querySelector('#mensaje');
-const formulario = document.querySelector('#error');
+const error = document.querySelector('#error');
+const validacionEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 eventListeners();
 
@@ -21,22 +22,33 @@ function iniciarApp() {
 
 function validarCampos(e) {
     if(!e.target.value) {
-        e.target.classList.remove('border-green-600')
-        e.target.classList.add('border-red-600')
-
-        mostrarError();
-
+        e.target.classList.remove('border-green-600');
+        e.target.classList.add('border-red-600');
+        mostrarError('Todos los campos son obligatorios');
     }else {
-        e.target.classList.remove('border-red-600')
-        e.target.classList.add('border-green-600')
+        e.target.classList.remove('border-red-600');
+        e.target.classList.add('border-green-600');
+        error.remove();
+    }
+
+    if(e.target.type === 'email') {
+        if(validacionEmail.test(e.target.value)) {
+            console.log('ok')
+            error.remove();
+            
+        } else {
+            e.target.classList.remove('border-green-600');
+            e.target.classList.add('border-red-600');
+            mostrarError('E-mail no valido');
+        }
     }
 }
 
-function mostrarError() {
-    formulario.innerHTML = '';
+function mostrarError(msj) {
+    error.innerHTML = '';
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = msj;
     mensajeError.classList.add('border', 'border-red-500', 'bg-red-700', 'text-white', 'p-3', 'mt-5', 'text-center');
-    formulario.appendChild(mensajeError);
+    error.appendChild(mensajeError);
 }
 
